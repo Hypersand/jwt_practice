e082ff6c59ccfc0d1a63297ebde3c5861903b45f
Hypersand <on8214@gmail.com>
2023-05-16 15:21:38 +0900

    accessToken 발급 및 테스트

--- src/test/java/com/example/jwt_practice/JwtTests.java
+++ src/test/java/com/example/jwt_practice/JwtTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,5 +56,19 @@ public class JwtTests {
         SecretKey secretKey2 = jwtProvider.getSecretKey();
 
         assertThat(secretKey1 == secretKey2).isTrue();
+    }
+
+    @Test
+    @DisplayName("accessToken 을 얻는다.")
+    void t5() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1L);
+        claims.put("username", "admin");
+
+        String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+
+        System.out.println("accessToken : " + accessToken);
+
+        assertThat(accessToken).isNotNull();
     }
 }
